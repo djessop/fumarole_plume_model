4789c1b9cf91cb20987401869727b3b1e12e7a4f
David Jessop <d.jessop@opgc.fr>
2024-02-08 16:13:11 +0100

    first commit

---
+++ data/ExpPlumes_for_Dai/GCTA_data_snapshots.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+GCTA_data_snapshots.py
+"""
+
+from bentPlumeAnalyser import *
+from scipy.io.matlab import loadmat
+from matplotlib.lines import Line2D
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    cols, rows = 7, 4
+    fig, axes = plt.subplots(ncols=cols, nrows=rows, 
+                             sharex=True, sharey=True, 
+                             figsize=(14, 8))
+
+    axes = axes.ravel()
+
+    ind = 0
+
+    for exptNo in range(1, 33):
+        path  = './exp%02d/' % exptNo
+        try:
+            ax, im, data, xexp, zexp, extent, Orig = openPlotExptImage(path,
+                                                                       axes,
+                                                                       exptNo,
+                                                                       ind)
+            Ox, Oz = Orig
+            ind += 1                # Only update axes index if expt exits
+        
+        except FileNotFoundError: # TypeError
+            continue
+            #print('file %s doesn\'t seem to exist...skipping!' % fname)
+
+    # Add a colorbar as separate axes
+    cbar_ax = fig.add_axes([.92, .15, .025, .7])
+    cbar = fig.colorbar(im, cax=cbar_ax)
+    cbar.set_label('Intensity')
+
+    # Legend in the final axes
+    custom_lines = [Line2D([0], [0], ls='-', c='w', lw=2),
+                    Line2D([0], [0], ls='--', c='w', lw=2)]
+    legend = axes[-1].legend(custom_lines, ['GCTA', 'Present'])
+    legend.get_frame().set_facecolor('#bbbbbb')
+
+    fig.suptitle(r'Carazzo \& Aubry expts')
+    fig.savefig('GCTA_expt_snapshots.pdf', bbox_inches=None)
+    fig.savefig('GCTA_expt_snapshots.png', bbox_inches=None)
+
+    plt.show()
