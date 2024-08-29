6928773fdf96be4a1bbacb540300fa677a5a01f8
David Jessop <d.jessop@opgc.fr>
2024-08-29 13:31:51 +0200

    Removed plumeAnalyserInversion2019051*{ipynb,py*}

--- plumemodel_for_thesis/plumemodel_for_thesis_header.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-from myiapws import iapws1995
-
-Tt = iapws1995.Tt  # Triple point of water
-
-
--- plumemodel_for_thesis/plumemodel_for_thesis_heat_capacity.py
+++
@@ -1,9 +0,0 @@
-def heat_capacity(s, V, ks=0.09, kw=0.9, g=9.81, Ca=1006, Cp0=1885,
-                  Ta0=291, Ra=287, Rp0=462, Pa0=86000, n0=0.05):
-    """
-    Define specific heat capacity, Cp, of plume, as a function of the dry air 
-      mass fraction, n, for initial 95% of vapour 
-    Ca:  spec. heat capacity of air
-    Cp0: spec. heat capacity of vapour
-    """  
-    return Ca + (Cp0 - Ca) * (1 - V[5]) / (1 - n0)
